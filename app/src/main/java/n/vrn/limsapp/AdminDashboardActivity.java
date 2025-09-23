1c23415b38d3d6211a195ce78a060e3d49d488a3
VRN-github <139988785+VRN-github@users.noreply.github.com>
2025-09-23 21:41:20 +0530

    Create AdminDashboardActivity.java

---
+++ app/src/main/java/n/vrn/limsapp/AdminDashboardActivity.java
@@ -0,0 +1,13 @@
+package n.vrn.limsapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminDashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+        // You can later add sample/test management here
+    }
+}
