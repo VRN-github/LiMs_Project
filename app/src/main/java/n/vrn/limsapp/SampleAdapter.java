5b0653d2837301b9ef4ac0a98b5d68a83d1aa17d
VRN-github <139988785+VRN-github@users.noreply.github.com>
2025-09-23 20:29:14 +0530

    Update SampleAdapter.java

--- app/src/main/java/n/vrn/limsapp/SampleAdapter.java
+++ app/src/main/java/n/vrn/limsapp/SampleAdapter.java
@@ -4,40 +4,54 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class SampleAdapter extends RecyclerView.Adapter<SampleAdapter.SampleViewHolder> {
-    private List<Sample> samples = new ArrayList<>();
+    private List<Sample> sampleList;
 
-    public void setSamples(List<Sample> samples) {
-        this.samples = samples;
+    public SampleAdapter(List<Sample> sampleList) {
+        this.sampleList = sampleList;
+    }
+
+    public void updateList(List<Sample> newList) {
+        sampleList = newList;
         notifyDataSetChanged();
     }
 
+    @NonNull
     @Override
-    public SampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public SampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sample, parent, false);
         return new SampleViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(SampleViewHolder holder, int position) {
-        holder.textView.setText(samples.get(position).name);
+    public void onBindViewHolder(@NonNull SampleViewHolder holder, int position) {
+        Sample sample = sampleList.get(position);
+        holder.name.setText(sample.name);
+        holder.type.setText("Type: " + sample.type);
+        holder.status.setText("Status: " + sample.status);
+        holder.timestamp.setText(new SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(sample.timestamp));
     }
 
     @Override
     public int getItemCount() {
-        return samples.size();
+        return sampleList.size();
     }
 
     static class SampleViewHolder extends RecyclerView.ViewHolder {
-        TextView textView;
-        SampleViewHolder(View itemView) {
+        TextView name, type, status, timestamp;
+
+        public SampleViewHolder(@NonNull View itemView) {
             super(itemView);
-            textView = itemView.findViewById(R.id.sampleName);
+            name = itemView.findViewById(R.id.sampleName);
+            type = itemView.findViewById(R.id.sampleType);
+            status = itemView.findViewById(R.id.sampleStatus);
+            timestamp = itemView.findViewById(R.id.sampleTimestamp);
         }
     }
 }
-
