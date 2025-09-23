b72199a8fb18a01473c9f0b35ec6b951228a01e1
VRN-github <139988785+VRN-github@users.noreply.github.com>
2025-09-23 19:40:31 +0530

    Update Sample.java

--- app/src/main/java/n/vrn/limsapp/Sample.java
+++ app/src/main/java/n/vrn/limsapp/Sample.java
@@ -4,4 +4,12 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
-public
+public class Sample {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String name;
+
+    public Sample(String name) {
+        this.name = name;
+    }
+}
