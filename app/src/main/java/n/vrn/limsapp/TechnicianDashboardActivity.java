607bf8a25ea770dcf4167ad019d638674a67492e
VRN-github <139988785+VRN-github@users.noreply.github.com>
2025-09-23 21:42:51 +0530

    Create TechnicianDashboardActivity.java

---
+++ app/src/main/java/n/vrn/limsapp/TechnicianDashboardActivity.java
@@ -0,0 +1,13 @@
+package n.vrn.limsapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TechnicianDashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_technician_dashboard);
+        // You can later show assigned samples/tests here
+    }
+}
